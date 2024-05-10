7567a5f49a0ae9a8d72c8278a107e6107fd59655
shahoodzaidi <zs83274@gmail.com>
2024-05-10 15:40:18 +0530

    Add all files

---
+++ new.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $(".scroll ").textillate({
+  
+      
+      in:{
+        effect:"flipInY",
+        delay:10,
+        
+  
+       
+      },
+      out:{
+        effect:"bounceOut",
+        delay:50,
+        
+       
+      }
+    })
+  })
+$(document).ready(function(){
+  $(".projec, #resume").textillate({
+    
+  })
+})
+
+  let nav = document.querySelector("ul")
+
+function full(){
+  nav.classList.toggle("full")
+}
+let nav1=document.querySelector("#nav")
+
